5fd8587e568a2e53e62d1fad93a9d4ed9a54d8af
David Naranjo <davidabrahamns@gmail.com>
2022-07-19 19:12:05 -0500

    theme color added and about me page

--- proyect-js/js/main.js
+++ proyect-js/js/main.js
@@ -3,50 +3,55 @@
 $(document).ready(function() {
 
     console.log('Welcome!');
-    $('.bxslider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        responsive: true
-    });
+    //slider
+    if (window.location.href.indexOf('index') > -1) { //this checks if the page is index
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200,
+            responsive: true
+        });
+    }
+
 
 
     //Posts
-    var posts = [{
-            title: 'Example Title',
-            date: new Date(),
-            content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
+    if (window.location.href.indexOf('index') > -1) { //this checks if the page is index
+        var posts = [{
+                title: 'Example Title',
+                date: new Date(),
+                content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
 
-        },
-        {
-            title: 'Example Title 2',
-            date: new Date(),
-            content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
+            },
+            {
+                title: 'Example Title 2',
+                date: new Date(),
+                content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
 
-        },
-        {
-            title: 'Example Title 3',
-            date: new Date(),
-            content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
+            },
+            {
+                title: 'Example Title 3',
+                date: new Date(),
+                content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
 
-        },
-        {
-            title: 'Example Title 4',
-            date: new Date(),
-            content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
+            },
+            {
+                title: 'Example Title 4',
+                date: new Date(),
+                content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
 
-        }, {
-            title: 'Example Title 5',
-            date: new Date(),
-            content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
+            }, {
+                title: 'Example Title 5',
+                date: new Date(),
+                content: 'Sed sed nisi enim. Mauris faucibus malesuada erat, sit amet maximus felis accumsan ac. Aenean vel enim dui. Morbi elit mauris, hendrerit et felis quis, gravida convallis lectus. Morbi semper vitae quam vitae gravida. In sem erat, consequat consectetur mi sed, volutpat tempus risus. Nunc laoreet dui nec neque accumsan pretium. In vitae congue quam.',
 
-        },
-    ]
+            },
+        ]
 
-    console.log(posts)
+        console.log(posts)
 
-    posts.forEach((item, index) => {
-        var post = `  <h2>${item.title}</h2>
+        posts.forEach((item, index) => {
+            var post = `  <h2>${item.title}</h2>
 
         <span class="date">${item.date}</span>
         <p>
@@ -56,7 +61,71 @@ $(document).ready(function() {
 
 `
 
-        $('#posts').append(post)
+            $('#posts').append(post)
+        });
+    }
+
+    //theme selector
+
+    var style = $("#theme");
+
+    $('#to-blue').click(function() {
+        style.attr('href', 'css/blue.css');
     });
+
+    $('#to-green').click(function() {
+        style.attr('href', 'css/green.css');
+    });
+
+    $('#to-red').click(function() {
+        style.attr('href', 'css/red.css');
+    });
+
+
+    //top scroll
+
+    $('.up').click(function(e) {
+        e.preventDefault();
+        $('html , body').animate({
+            scrollTop: 0
+        }, 500);
+
+        return false;
+    });
+
+    //contact info
+
+    $('#login').submit(function() {
+        var formName = $('#form-name').val();
+        localStorage.setItem("form-name", formName);
+
+    });
+
+    var formName = localStorage.getItem("form-name");
+
+    if (formName != null && formName != "undefined") {
+
+        var about = $('#about');
+        about.html("<strong>Welcome , " + formName + " !</strong>,you can contact me in here https://github.com/DANS03");
+
+
+        about.append("<br/><a href='#' id='logout'>Good Bye!</a>");
+
+        $('#login').hide();
+
+        $('#logout').click(function() {
+            localStorage.clear();
+            location.reload(); // refresh the window
+        });
+
+    }
+
+    //move it into another file
+    if (window.location.href.indexOf('about') > -1) {
+        $('#accordion').accordion();
+    }
+
+
+
 
 });
