a62754e5d05663609862cdc907ab63810cecd769
David Naranjo <davidabrahamns@gmail.com>
2022-07-22 15:53:51 -0500

    Dependencies and Hello world from TypeScript

---
+++ oop-and-typescript/js/02-hello-world.ts
@@ -0,0 +1,9 @@
+console.log('Hello World from TypeScript');
+
+//in order to have a constant flow of coding we can put tsc in watch mode using this command "tsc -w *.ts" in order to work you mut do it in a linux terminal
+
+
+alert('watch mode enable');
+
+console.log('changes');
+
