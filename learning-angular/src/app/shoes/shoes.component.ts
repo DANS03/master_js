bd2d97337a8dd77bb3a0c58a2d346f2b57190533
David Naranjo <davidabrahamns@gmail.com>
2022-08-03 00:34:17 -0500

    Directives

--- learning-angular/src/app/shoes/shoes.component.ts
+++ learning-angular/src/app/shoes/shoes.component.ts
@@ -4,17 +4,20 @@ import { Shoe } from "../models/shoe"
 @Component({
     selector: 'shoe',
     templateUrl: './shoes.component.html'
+    
 })
 export class ShoesComponent implements OnInit{
     public title :string = "Shoe Component";
     public shoes : Array<Shoe>;
+    public color : 'blue'; 
 
     constructor(){
+        this.color = 'blue';
         this.shoes = [
             new Shoe('Air One' , 100 , true , 'blue' , 'nike'),
-            new Shoe('Air two' , 100 , true , 'blue' , 'adidas'),
+            new Shoe('Air two' , 80 , true , 'blue' , 'adidas'),
             new Shoe('Air three' , 100 , true , 'blue' , 'vans'),
-            new Shoe('Air four' , 100 , true , 'blue' , 'converse')
+            new Shoe('Air four' , 60 , true , 'blue' , 'converse')
 
         ];
     }
