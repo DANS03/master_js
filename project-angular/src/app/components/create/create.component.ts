b46a4952190cb5670f9b91ec17609ce80d0c1256
David Naranjo <davidabrahamns@gmail.com>
2022-08-14 23:02:05 -0500

    Create project page design

--- project-angular/src/app/components/create/create.component.ts
+++ project-angular/src/app/components/create/create.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+  providers:[ProjectService]
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public project: Project;
+
+  constructor(
+    private _projectsSeervice : ProjectService
+
+  ) { 
+    this.title ='Create a Project';
+    this.project = new Project('','','','',2022,'','');
+
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit( form:any ){
+      console.log(this.project)
   }
 
 }
